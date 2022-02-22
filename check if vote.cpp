ac6c97f3de6e6ba6626e8c30bb76761c1788b642
techiegarima <80690565+techiegarima@users.noreply.github.com>
2022-02-22 08:01:57 +0530

    Create check if vote.cpp

---
+++ check if vote.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age;
+  cout<<"Enter the age:"<<endl;
+  cin>>age;
+  if(age>=18)
+    cout<<"\n You are elligible";
+  else
+     cout<<"\n You are not elligible";
+     return 0;
+ }    
